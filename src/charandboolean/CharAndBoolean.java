82bbe782f1fb2d18613ec906c4d9f3148436cf9a
tyler <tyler@LAPTOP-FNAAGTKI>
2021-02-04 11:18:27 +0000

    FarmYard program class worked cleaned and finished.

---
+++ src/charandboolean/CharAndBoolean.java
@@ -0,0 +1,27 @@
+
+package charandboolean;
+
+public class CharAndBoolean {
+
+  
+    public static void main(String[] args) {
+        
+        char myChar  ='D';
+        char myUniCode = '\u0044';
+        System.out.println(myChar + " " + myUniCode);
+        char myCopyRight = '\u00A9';
+        System.out.println(myCopyRight);
+        boolean myTruBooleanValue = true;
+        boolean myFalsoBooleanValue = false;
+        
+        
+        boolean isCustomerOverTwentyOne = true;
+        
+        System.out.println(myChar);
+        System.out.println(myUniCode);
+        System.out.println(myCopyRight);
+        System.out.println(isCustomerOverTwentyOne);
+      
+    }
+    
+}
